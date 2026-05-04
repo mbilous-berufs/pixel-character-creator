2f2c31cd9e45ea71a8d1053ab33fdeb1d8c611f6
Mariia Bilous <>
2026-05-04 15:15:41 +0200

    Initial Commit

---
+++ src/main/java/ch/mariia/bilous/pixelcharactercreator/pixelcharactercreator/PixelCharacterCreatorApplication.java
@@ -0,0 +1,13 @@
+package ch.mariia.bilous.pixelcharactercreator.pixelcharactercreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelCharacterCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixelCharacterCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/mariia/bilous/pixelcharactercreator/pixelcharactercreator/PixelCharacterCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.mariia.bilous.pixelcharactercreator.pixelcharactercreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelCharacterCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
