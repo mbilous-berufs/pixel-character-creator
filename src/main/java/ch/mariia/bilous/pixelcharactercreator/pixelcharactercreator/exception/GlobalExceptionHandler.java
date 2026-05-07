ae8cc9d178f866292d64a7543c4f38e79449f1eb
Mariia Bilous <>
2026-05-07 15:21:57 +0200

    Add global exception handling: validation and not found errors

---
+++ src/main/java/ch/mariia/bilous/pixelcharactercreator/pixelcharactercreator/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package ch.mariia.bilous.pixelcharactercreator.pixelcharactercreator.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException exception) {
+        return ResponseEntity.badRequest().body("Validation failed");
+    }
+}
---
+++ src/main/java/ch/mariia/bilous/pixelcharactercreator/pixelcharactercreator/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ch.mariia.bilous.pixelcharactercreator.pixelcharactercreator.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
