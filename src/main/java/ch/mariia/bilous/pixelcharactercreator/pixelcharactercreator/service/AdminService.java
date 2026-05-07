7922238a9a4c684a11aae496d350a1547e9c9880
Mariia Bilous <>
2026-05-07 11:37:07 +0200

    Add Admin service

---
+++ src/main/java/ch/mariia/bilous/pixelcharactercreator/pixelcharactercreator/service/AdminService.java
@@ -0,0 +1,10 @@
+package ch.mariia.bilous.pixelcharactercreator.pixelcharactercreator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+    public String getAdminInfo() {
+        return "Admin area for global customization management";
+    }
+}
